bbcedd9f47b91a3bca1b597f6b583df9695cc5aa
redyyu <redy.ru@gmail.com>
2024-03-24 17:03:41 +0800

    xxx

---
+++ media/lua/server/Items/ATACommonItemDistributions.lua
@@ -0,0 +1,33 @@
+require 'Items/ProceduralDistributions'
+
+    -- Base.ATAProtectionWheelsChain
+    table.insert(ProceduralDistributions.list["CrateMechanics"].items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["CrateMechanics"].items, 2);
+    table.insert(ProceduralDistributions.list["GarageMechanics"].items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["GarageMechanics"].items, 2);
+    table.insert(ProceduralDistributions.list["MechanicShelfMisc"].items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["MechanicShelfMisc"].items, 2);
+    table.insert(ProceduralDistributions.list["MechanicShelfTools"].items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["MechanicShelfTools"].items, 2);
+    table.insert(ProceduralDistributions.list["MechanicShelfTools"].junk.items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["MechanicShelfTools"].junk.items, 2);
+    table.insert(ProceduralDistributions.list["MechanicShelfWheels"].items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["MechanicShelfWheels"].items, 2);
+    table.insert(ProceduralDistributions.list["CrateMetalwork"].items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["CrateMetalwork"].items, 2);
+    table.insert(ProceduralDistributions.list["GarageMetalwork"].items, "Base.ATAProtectionWheelsChain");
+    table.insert(ProceduralDistributions.list["GarageMetalwork"].items, 2);
+    
+    -- Base.ATARoofLightItem
+    table.insert(ProceduralDistributions.list["CrateMechanics"].items, "Base.ATARoofLightItem");
+    table.insert(ProceduralDistributions.list["CrateMechanics"].items, 2);
+    table.insert(ProceduralDistributions.list["GarageMechanics"].items, "Base.ATARoofLightItem");
+    table.insert(ProceduralDistributions.list["GarageMechanics"].items, 2);
+    table.insert(ProceduralDistributions.list["GarageMetalwork"].items, "Base.ATARoofLightItem");
+    table.insert(ProceduralDistributions.list["GarageMetalwork"].items, 2);
+    table.insert(ProceduralDistributions.list["MechanicShelfMisc"].items, "Base.ATARoofLightItem");
+    table.insert(ProceduralDistributions.list["MechanicShelfMisc"].items, 2);
+    table.insert(ProceduralDistributions.list["MechanicShelfElectric"].items, "Base.ATARoofLightItem");
+    table.insert(ProceduralDistributions.list["MechanicShelfElectric"].items, 2);
+    table.insert(ProceduralDistributions.list["MechanicShelfElectric"].junk.items, "Base.ATARoofLightItem");
+    table.insert(ProceduralDistributions.list["MechanicShelfElectric"].junk.items, 2);
